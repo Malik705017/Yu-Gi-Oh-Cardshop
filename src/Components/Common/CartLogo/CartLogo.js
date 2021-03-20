10d501479f3489b234c90f077baaac1cd40c45d4
Malik <fhh1482@gmail.com>
2021-03-20 23:26:20 +0800

    add cart logo

---
+++ src/Components/Common/CartLogo/CartLogo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const cartIconStyle = {
+    maxWidth:'30px'
+}
+const cartLogo = (props) =>  <img style = {cartIconStyle} alt = "購物車" src= {props.src}/>
+
+export default cartLogo
